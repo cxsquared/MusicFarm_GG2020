b7a8c9cbd246fa4e4355fc7f6c1d8cd9f4bb3f74
Travis <travis.faas@gmail.com>
2020-01-31 23:28:16 -0500

    Character movement, start of flower dropper

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,12 +1,26 @@
 package;
 
 import flixel.FlxState;
+import characters.Player;
+import ui.FlowerPicker;
 
 class PlayState extends FlxState
 {
+
+	private var player:Player;
+	private var fp:FlowerPicker;
+
 	override public function create():Void
 	{
 		super.create();
+
+		//Cluttering things up because that's how I am
+		player = new Player();
+		add(player);
+
+		fp = new FlowerPicker();
+		add(fp);
+
 	}
 
 	override public function update(elapsed:Float):Void
