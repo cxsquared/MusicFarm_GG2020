9053186f4128cd74fc9101f424482944334a431e
Cody Claborn <cxsquared@gmail.com>
2020-02-02 13:54:04 -0500

    Added twitters

--- source/StoryState.hx
+++ source/StoryState.hx
@@ -1,3 +1,5 @@
+import flixel.util.FlxColor;
+import flixel.text.FlxText;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
@@ -6,6 +8,8 @@ import flixel.util.FlxTimer;
 class StoryState extends FlxState {
     var bg = new FlxSprite();
     var fg = new FlxSprite();
+    var cx = new FlxText();
+    var travis = new FlxText();
 
     var bgs = ["assets/images/story_bg1.png",
                 "assets/images/story_bg2.png",
@@ -41,6 +45,23 @@ class StoryState extends FlxState {
         add(fg);
 
         fgTimer.start(FlxG.random.float(2,4), switchForeground);
+
+        cx = new FlxText();
+        cx.text = "@cxsquared";
+        cx.color = FlxColor.WHITE;
+        cx.size = 28;
+        cx.x = 8;
+        cx.y = FlxG.height - cx.height - 8;
+        add(cx);
+
+        travis = new FlxText();
+        travis.text = "@meanderingleaf";
+        travis.color = FlxColor.WHITE;
+        travis.size = 28;
+        travis.x = FlxG.width - travis.width - 8;
+        travis.y = FlxG.height - travis.height - 8;
+        add(travis);
+
     }
 
     override public function update(delta:Float) {
